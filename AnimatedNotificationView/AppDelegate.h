3bf1de9f09e8f06d1ecde8716227e7eacdfd3c8f
Jon Friskics <jonfriskics@gmail.com>
2013-06-29 22:19:01 -0400

    Initial Commit

---
+++ AnimatedNotificationView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnimatedNotificationView
+//
+//  Created by Jon Friskics on 6/29/13.
+//  Copyright (c) 2013 Jon Friskics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
